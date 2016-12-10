3befb0d2a7f2c4c798628f46b10f9d2fbb84a701
svustenyuk <svustenuk@gmail.com>
2016-12-10 22:35:59 +0200

    sss

--- JS/factorys/listsFactory.js
+++ JS/factorys/listsFactory.js
@@ -1,35 +1,49 @@
 /**
  * Created by Sasha on 03.12.2016.
  */
-angular.module('app').factory('listsFactory', function(){
+angular.module('app').factory('listsFactory', function($http){
     var service = {};
-   var lists = [
-        {
-            id: 1,
-            listName: 'Todo'
-        },
-        {
-            id: 2,
-            listName: 'Doing'
-        },
-        {
-            id: 3,
-            listName: 'Done'
-        }
-    ];
 
-    service.getLists = function(){
-        return lists;
+
+
+
+        /* $http.get('http://localhost:3001/lists')
+            .success(function(result){
+
+                service.data=result;
+
+            })
+            .error(function(result){
+                console.log('error listsFactory');
+            });*/
+
+    service.getLists=function(){
+        return $http.get('http://localhost:3001/lists');
     };
 
+
+
+
+
     service.addList = function (listName){
-        lists.push({
+        console.log(listName);
+        $http.post('http://localhost:3001/lists', listName)
+            .success(function(result){
+
+            })
+            .error(function(result){
+                console.log('error listsFactory', result);
+            });
+
+
+        /*lists.push({
             id: _.uniqueId('list_ '),
             listName: listName
-        });
+        });*/
     };
     service.removeList = function(list){
         _.pull(lists, list);
     };
+
     return service;
 });
