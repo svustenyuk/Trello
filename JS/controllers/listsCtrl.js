11c9aca0bb0debbf853dd64a47bdbe5a82b38742
svustenyuk <svustenuk@gmail.com>
2017-03-01 22:28:51 +0200

    jddj

--- JS/controllers/listsCtrl.js
+++ JS/controllers/listsCtrl.js
@@ -2,7 +2,7 @@
  * Created by Sasha on 03.12.2016.
  */
 angular.module('app').controller('listsCtrl', function(listsFactory){
-    console.log('listsCtrl');
+    
     this.lists = listsFactory.getLists();
     
     this.addList = function () {
