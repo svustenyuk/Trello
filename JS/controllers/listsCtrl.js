918ffb1fde1f48d66f031fac39881b78c9363325
svustenyuk <svustenuk@gmail.com>
2017-03-01 22:03:20 +0200

    jddj

--- JS/controllers/listsCtrl.js
+++ JS/controllers/listsCtrl.js
@@ -1,42 +1,13 @@
 /**
  * Created by Sasha on 03.12.2016.
  */
-angular.module('app').controller('listsCtrl', function($scope, listsFactory){
-
-   listsFactory.getLists().success(function(result){
-        $scope.lists=result;
-        })
-        .error(function(result){
-
-    });
-
-
-
-
+angular.module('app').controller('listsCtrl', function(listsFactory){
+    console.log('listsCtrl');
+    this.lists = listsFactory.getLists();
+    
     this.addList = function () {
-        console.log(this.listName);
         listsFactory.addList(this.listName);
+        this.listName='';
     };
-
-
-
-
-
-      // listsFactory.addList(this.listName);
-      //  this.listName='';
-
-
-   /* $http.get('http://localhost:3001/lists')
-        .success(function(result){
-
-            this.lists= result;
-
-        })
-        .error(function(result){
-            console.log('error listsFactory');
-        });*/
-
-
-
 
 });
--- JS/factorys/listsFactory.js
+++ JS/factorys/listsFactory.js
@@ -1,49 +1,35 @@
 /**
  * Created by Sasha on 03.12.2016.
  */
-angular.module('app').factory('listsFactory', function($http){
+angular.module('app').factory('listsFactory', function(){
     var service = {};
+   var lists = [
+        {
+            id: 1,
+            listName: 'Todo'
+        },
+        {
+            id: 2,
+            listName: 'Doing'
+        },
+        {
+            id: 3,
+            listName: 'Done'
+        }
+    ];
 
-
-
-
-        /* $http.get('http://localhost:3001/lists')
-            .success(function(result){
-
-                service.data=result;
-
-            })
-            .error(function(result){
-                console.log('error listsFactory');
-            });*/
-
-    service.getLists=function(){
-        return $http.get('http://localhost:3001/lists');
+    service.getLists = function(){
+        return lists;
     };
 
-
-
-
-
     service.addList = function (listName){
-        console.log(listName);
-        $http.post('http://localhost:3001/lists', listName)
-            .success(function(result){
-
-            })
-            .error(function(result){
-                console.log('error listsFactory', result);
-            });
-
-
-        /*lists.push({
+        lists.push({
             id: _.uniqueId('list_ '),
             listName: listName
-        });*/
+        });
     };
     service.removeList = function(list){
         _.pull(lists, list);
     };
-
     return service;
 });
